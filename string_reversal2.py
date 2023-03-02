d730ab675b1bd17de380b8a5b0c9e6664d4af711
Shiva Kumar <82926643+ShivaKumar-Kola@users.noreply.github.com>
2023-03-02 10:18:47 +0530

    Create string_reversal2.py

---
+++ string_reversal2.py
@@ -0,0 +1,9 @@
+#reversal of string using recursion
+
+def reverse(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a = str(input("Enter a string : "))
+print(reverse(a))
